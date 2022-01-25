2c6ce1c511fb5f10853279d3610b23d1d375bd23
emmanuelkiranr <emmanuelkiranr@gmail.com>
2022-01-26 01:05:36 +0530

    added basic unit tests

--- contracts/Hello.sol
+++ contracts/Hello.sol
@@ -5,9 +5,13 @@ pragma solidity ^0.8.0;
 contract Hello {
     string private message;
 
-    // constructor(string memory _message) {
-    //     message = _message;
-    // }
+    constructor(string memory _message) {
+        message = _message;
+    }
+
+    function setter() public {
+        message = "My first smart contract";
+    }
 
     function setMessage(string memory _message) public {
         message = _message;
--- test/Hello.test.js
+++ test/Hello.test.js
@@ -1,1 +1,22 @@
-// test for the Hello.sol contract
+// // test for the Hello.sol contract
+
+const { expect } = require("chai");
+// const { ethers } = require("ethers")
+
+describe("Unit testing Hello.sol", function() {
+  it("The message should be same", async function() {
+    const Hello = await ethers.getContractFactory("Hello");
+    const hello = await Hello.deploy("Unit testing");
+    await hello.deployed();
+    expect (await hello.getMessage()).to.equal("Unit testing");
+  });
+
+  it("Should update the value", async function() {
+    const Hello = await ethers.getContractFactory("Hello");
+    const hello = await Hello.deploy("Unit testing");
+    await hello.deployed();
+
+    await hello.setMessage("Message updated");
+    expect(await hello.getMessage()).to.equal("Message updated");
+  })
+});
