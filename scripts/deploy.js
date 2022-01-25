01029acec72910a5bc11912e27377a0ec086fe94
emmanuelkiranr <emmanuelkiranr@gmail.com>
2022-01-25 14:29:08 +0530

    created and deployed contract to hardhat network

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,15 @@
 // contains the deploy script to deploy the contract
+
+async function main() {
+  const Hello = await ethers.getContractFactory("Hello");
+  const hello = await Hello.deploy(); 
+  await hello.deployed(); 
+  // console.log("Contract deployed to:", hello.address); 
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
