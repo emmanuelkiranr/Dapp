b43febabb6031d46331b57a2bba27a59c4043952
emmanuelkiranr <emmanuelkiranr@gmail.com>
2022-02-01 19:08:13 +0530

    updated unit testing, added solidity-coverage

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 // require('@nomiclabs/hardhat-ethers');
 require("@nomiclabs/hardhat-waffle");
+require("solidity-coverage");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
--- test/Hello.test.js
+++ test/Hello.test.js
@@ -3,20 +3,28 @@
 const { expect } = require("chai");
 // const { ethers } = require("ethers")
 
-describe("Unit testing Hello.sol", function() {
-  it("The message should be same", async function() {
-    const Hello = await ethers.getContractFactory("Hello");
-    const hello = await Hello.deploy("Unit testing");
+describe("Hello Contract", function() {
+  let Hello, hello;
+  
+  beforeEach(async function() {
+    Hello = await ethers.getContractFactory("Hello");
+    hello = await Hello.deploy("Unit testing");
     await hello.deployed();
-    expect (await hello.getMessage()).to.equal("Unit testing");
   });
 
-  it("Should update the value", async function() {
-    const Hello = await ethers.getContractFactory("Hello");
-    const hello = await Hello.deploy("Unit testing");
-    await hello.deployed();
+  describe("Unit testing Hello.sol", function() {
+    it("The message should be same", async function() {
+      expect (await hello.getMessage()).to.equal("Unit testing");
+    });
 
-    await hello.setMessage("Message updated");
-    expect(await hello.getMessage()).to.equal("Message updated");
-  })
+    it("Should update the value", async function() {
+      await hello.setMessage("Message updated");
+      expect(await hello.getMessage()).to.equal("Message updated");
+    });
+
+    it("Should initialize with the value in setter method", async function(){
+      await hello.setter();
+      expect(await hello.getMessage()).to.equal("My first smart contract");
+    });
+  });
 });
